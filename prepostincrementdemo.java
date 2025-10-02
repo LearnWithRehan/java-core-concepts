de010012ffdda5b7504b37646f604edb251a7add
LearnWithRehan <learnwithrehan86147@gmail.com>
2025-10-02 23:37:46 +0530

    some add code

---
+++ prepostincrementdemo.java
@@ -0,0 +1,10 @@
+public class prepostincrementdemo {
+    public static void main(String[] args) {
+        int x = 10;
+        System.out.println(x);
+        System.out.println(x++);
+        System.out.println(++x);
+        System.out.println(x--);
+        System.out.println(--x);
+    }
+}
