2ddedeaec16e77dd404c226021cedaf2e925374d
LearnWithRehan <learnwithrehan86147@gmail.com>
2025-10-07 23:13:50 +0530

    add some file

---
+++ CountWords.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class CountWords {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Sentence:- ");
+        String sentence = sc.nextLine().trim();
+        if (sentence.isEmpty()){
+            System.out.println("Number of words:- 0");
+        }else {
+            String[] words = sentence.split("\\s+");
+            System.out.println("Number of words:- " + words.length);
+        }
+    }
+
+}
