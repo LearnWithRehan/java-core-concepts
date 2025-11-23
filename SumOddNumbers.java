5b9252bc0b0163a628dd7df74a0f5b9b4ed3969b
LearnWithRehan <learnwithrehan86147@gmail.com>
2025-11-23 14:44:45 +0530

    Sum Odd Numbers

--- SumOddNumbers.java
+++ SumOddNumbers.java
@@ -1,11 +1,14 @@
 import java.util.Scanner;
+
 public class SumOddNumbers {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter n: ");
-        int n=sc.nextInt(), sum=0;
-        for (int i = 1,count=0;count<n;i +=2,count++)
-            sum +=i;
-        System.out.println("Sum of first " + n + " odd numbers = " + sum);
+        System.out.println("Enter Number:- ");
+        int n = sc.nextInt();
+        int sum = 0;
+        for (int i = 1, count=0;count<n;i +=2,count++){
+            sum += i;
+        }
+        System.out.println("Sum of Odd Number:- " + sum);
     }
 }
