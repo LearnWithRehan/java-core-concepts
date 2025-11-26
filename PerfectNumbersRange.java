de387b9e4e931fd1c801c2a1d49fd78fc23362b1
LearnWithRehan <learnwithrehan86147@gmail.com>
2025-11-27 00:24:43 +0530

    add some code

---
+++ PerfectNumbersRange.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class PerfectNumbersRange {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n: ");
+        int n=sc.nextInt();
+        System.out.println("Perfect numbers from 1 to " + n + ":");
+        for(int i=1;i<=n;i++){
+            int sum=0;
+            for(int j=1;j<i;j++) if(i%j==0) sum+=j;
+            if(sum==i) System.out.print(i + " ");
+        }
+    }
+}
