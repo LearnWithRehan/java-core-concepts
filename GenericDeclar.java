950f3c8ad603775880690d48196e8cd08341dedc
LearnWithRehan <learnwithrehan86147@gmail.com>
2025-12-04 17:19:13 +0530

    Generic Declare and Calculator

--- GenericDeclar.java
+++ GenericDeclar.java
@@ -1,21 +1,18 @@
-class Box<T>{
-    private T value;
-    void set(T value){
-        this.value = value;
-    }
-    T get(){
-        return value;
+public class GenericDeclar {
+    public static void main(String[] args) {
+
+        Box<Integer> intBox = new Box<>(100);
+        Box<String> strBox = new Box<>("Hello Java");
+
+        System.out.println("Integer Box: " + intBox.value);
+        System.out.println("String Box: " + strBox.value);
+
     }
 }
 
-public class GenericDeclar {
-    public static void main(String[] args) {
-        Box<Integer> intBox = new Box<>();
-        intBox.set(10);
-        System.out.println(intBox.get());
-        Box<String > strBox = new Box<>();
-        strBox.set("LearnWithRehan");
-        System.out.println(strBox.get());
+class Box<T>{
+    T value;
+    Box(T value){
+        this.value = value;
     }
-
 }
